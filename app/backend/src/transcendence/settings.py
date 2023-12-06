15178d9de384e90692b1a351d2bdcd801a2bfa7a
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-12-06 12:49:36 +0100

    Create form to create tournament - Update endpoint url - Add option APPEND_SLASH=False

--- app/backend/src/transcendence/settings.py
+++ app/backend/src/transcendence/settings.py
@@ -136,3 +136,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH=False
