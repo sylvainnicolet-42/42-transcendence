e9ebf45b64abd12f2c9700f837e37ef58752b157
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-12-07 13:40:25 +0100

    All authentification endpoints start with auth/

--- app/backend/src/api/urls.py
+++ app/backend/src/api/urls.py
@@ -23,10 +23,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('register', views.RegisterView.as_view(), name='register'),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('auth/register', views.RegisterView.as_view(), name='register'),
     path('hello', views.HelloWorldView.as_view(), name='hello'),
     path('tournaments/list', views.TournamentListView.as_view(), name='tournament-list'),
     path('tournaments/detail/(?P<pk>[0-9]+)', views.TournamentDetailView.as_view(), name='tournament-detail'),
