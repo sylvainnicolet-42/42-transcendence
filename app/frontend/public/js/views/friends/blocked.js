1826fa2f35f0e949247ff67a707d453d53dd4cef
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-12-14 10:34:58 +0100

    Add simple table to each tab

---
+++ app/frontend/public/js/views/friends/blocked.js
@@ -0,0 +1,47 @@
+async function blocked() {
+
+  // TODO: Get the list friends
+
+  const list = [];
+
+  const container = document.getElementById('friends_blocked_container');
+
+  // Create a table
+  const table = document.createElement('table');
+  table.className = 'table table-striped table-bordered table-hover';
+  container.appendChild(table);
+
+  // Create the table header
+  const thead = document.createElement('thead');
+  table.appendChild(thead);
+  const tr = document.createElement('tr');
+  thead.appendChild(tr);
+  const headers = ['Avatar', 'Username', 'Actions'];
+  headers.forEach(header => {
+    const th = document.createElement('th');
+    tr.appendChild(th);
+    th.textContent = header;
+    if (header === 'Actions') {
+      th.className = 'text-end';
+    }
+  });
+
+  // Create the table body
+  const tbody = document.createElement('tbody');
+  table.appendChild(tbody);
+
+  if (list.length === 0) {
+    const tr = document.createElement('tr');
+    tbody.appendChild(tr);
+    const td = document.createElement('td');
+    td.setAttribute('colspan', headers.length.toString());
+    td.textContent = 'No friends found';
+    tr.appendChild(td);
+  }
+}
+
+async function init() {
+  await blocked();
+}
+
+init();
--- app/frontend/public/js/views/friends/friends.js
+++ app/frontend/public/js/views/friends/friends.js
@@ -1,130 +1,47 @@
-// async function friends() {
-//   // TODO: Get the list friends
-//
-//   const list = [];
-//
-//   const container = document.getElementById('friends_container');
-//
-//   // Create a table
-//   const table = document.createElement('table');
-//   table.className = 'table table-striped table-bordered table-hover';
-//   container.appendChild(table);
-//
-//   // Create the table header
-//   const thead = document.createElement('thead');
-//   table.appendChild(thead);
-//   const tr = document.createElement('tr');
-//   thead.appendChild(tr);
-//   const headers = ['Avatar', 'Username', 'Points', 'Status', 'Actions'];
-//   headers.forEach(header => {
-//     const th = document.createElement('th');
-//     tr.appendChild(th);
-//     th.textContent = header;
-//     if (header === 'Actions') {
-//       th.className = 'text-end';
-//     }
-//   });
-//
-//   // Create the table body
-//   const tbody = document.createElement('tbody');
-//   table.appendChild(tbody);
-//
-//   if (list.length === 0) {
-//     const tr = document.createElement('tr');
-//     tbody.appendChild(tr);
-//     const td = document.createElement('td');
-//     td.setAttribute('colspan', headers.length.toString());
-//     td.textContent = 'No friends found';
-//     tr.appendChild(td);
-//   }
-// }
-//
-// async function friendRequestReceived() {
-//   // TODO: Get the list friend requests received
-//
-//   const list = [];
-//
-//   const container = document.getElementById('friend_requests_received_container');
-//
-//   // Create a table
-//   const table = document.createElement('table');
-//   table.className = 'table table-striped table-bordered table-hover';
-//   container.appendChild(table);
-//
-//   // Create the table header
-//   const thead = document.createElement('thead');
-//   table.appendChild(thead);
-//   const tr = document.createElement('tr');
-//   thead.appendChild(tr);
-//   const headers = ['Avatar', 'Username', 'Points', 'Status', 'Actions'];
-//   headers.forEach(header => {
-//     const th = document.createElement('th');
-//     tr.appendChild(th);
-//     th.textContent = header;
-//     if (header === 'Actions') {
-//       th.className = 'text-end';
-//     }
-//   });
-//
-//   // Create the table body
-//   const tbody = document.createElement('tbody');
-//   table.appendChild(tbody);
-//
-//   if (list.length === 0) {
-//     const tr = document.createElement('tr');
-//     tbody.appendChild(tr);
-//     const td = document.createElement('td');
-//     td.setAttribute('colspan', headers.length.toString());
-//     td.textContent = 'No friend requests received';
-//     tr.appendChild(td);
-//   }
-// }
-//
-// async function friendRequestSent() {
-//   // TODO: Get the list friend requests sent
-//
-//   const list = [];
-//
-//   const container = document.getElementById('friend_requests_sent_container');
-//
-//   // Create a table
-//   const table = document.createElement('table');
-//   table.className = 'table table-striped table-bordered table-hover';
-//   container.appendChild(table);
-//
-//   // Create the table header
-//   const thead = document.createElement('thead');
-//   table.appendChild(thead);
-//   const tr = document.createElement('tr');
-//   thead.appendChild(tr);
-//   const headers = ['Avatar', 'Username', 'Points', 'Status', 'Actions'];
-//   headers.forEach(header => {
-//     const th = document.createElement('th');
-//     tr.appendChild(th);
-//     th.textContent = header;
-//     if (header === 'Actions') {
-//       th.className = 'text-end';
-//     }
-//   });
-//
-//   // Create the table body
-//   const tbody = document.createElement('tbody');
-//   table.appendChild(tbody);
-//
-//   if (list.length === 0) {
-//     const tr = document.createElement('tr');
-//     tbody.appendChild(tr);
-//     const td = document.createElement('td');
-//     td.setAttribute('colspan', headers.length.toString());
-//     td.textContent = 'No friend requests sent';
-//     tr.appendChild(td);
-//   }
-// }
-//
-// async function init() {
-//   await friends();
-//   await friendRequestReceived();
-//   await friendRequestSent();
-// }
-//
-// init();
+async function friends() {
+
+  // TODO: Get the list friends
+
+  const list = [];
+
+  const container = document.getElementById('friends_container');
+
+  // Create a table
+  const table = document.createElement('table');
+  table.className = 'table table-striped table-bordered table-hover';
+  container.appendChild(table);
+
+  // Create the table header
+  const thead = document.createElement('thead');
+  table.appendChild(thead);
+  const tr = document.createElement('tr');
+  thead.appendChild(tr);
+  const headers = ['Avatar', 'Username', 'Points', 'Status', 'Actions'];
+  headers.forEach(header => {
+    const th = document.createElement('th');
+    tr.appendChild(th);
+    th.textContent = header;
+    if (header === 'Actions') {
+      th.className = 'text-end';
+    }
+  });
+
+  // Create the table body
+  const tbody = document.createElement('tbody');
+  table.appendChild(tbody);
+
+  if (list.length === 0) {
+    const tr = document.createElement('tr');
+    tbody.appendChild(tr);
+    const td = document.createElement('td');
+    td.setAttribute('colspan', headers.length.toString());
+    td.textContent = 'No friends found';
+    tr.appendChild(td);
+  }
+}
+
+async function init() {
+  await friends();
+}
+
+init();
